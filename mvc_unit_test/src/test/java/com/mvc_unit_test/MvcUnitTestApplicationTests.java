faf3b250b1ccb3ae0974b146a7f43b62384b7161
Vishal-1007 <vg03122001@gmail.com>
2025-12-30 18:20:57 +0530

    new unit testing started

---
+++ mvc_unit_test/src/test/java/com/mvc_unit_test/MvcUnitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvc_unit_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcUnitTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
